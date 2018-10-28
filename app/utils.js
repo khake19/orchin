74ab255704846b93a529cc15caa53cb39b8013aa
kerk <ed@acudeen.com>
2018-10-28 22:17:43 +0800

    add play method to utils

--- app/utils.js
+++ app/utils.js
@@ -1,5 +1,21 @@
+import defaultSounds from '../static/sounds/default';
+
 const capitalizeFirstLetter = string =>
   string.charAt(0).toUpperCase() + string.slice(1);
-const initial = () => '';
 
-export { capitalizeFirstLetter, initial };
+const play = name => {
+  const appMute = false;
+  if (!appMute) {
+    const sound = defaultSounds[name];
+    if (!sound) {
+      throw new Error('Invalid sound name');
+    }
+    const audio = new window.Audio();
+    audio.volume = sound.volume;
+    audio.src = sound.url;
+
+    audio.currentTime = 0;
+    audio.play();
+  }
+};
+export { capitalizeFirstLetter, play };
