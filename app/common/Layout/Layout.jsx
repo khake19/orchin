ac7afdfa2922e8a025033d4873f810a168453022
kerk <ed@acudeen.com>
2018-11-06 16:26:22 +0800

    use formik in user form

--- app/common/Layout/Layout.jsx
+++ app/common/Layout/Layout.jsx
@@ -1,59 +1,32 @@
-// Libraries
-import React from 'react';
-
-// Styles
 import styled from 'styled-components';
 
-const AppWrapperStyle = styled.div`
+const ErrorLabel = styled.div`
+  color: #f75858;
+  margin: 0.5rem 0 0 0.5rem;
+  font-size: 12px;
+`;
+
+const AppWrapper = styled.div`
   display: flex;
   flex-direction: row;
   height: 100%;
 `;
 
-const AppMainContentStyle = styled.div`
+const AppMainContent = styled.div`
   overflow: auto;
   width: 100%;
   height: 100%;
   background: #f9fafa;
 `;
 
-const PageWrapperStyle = styled.div`
+const PageWrapper = styled.div`
   overflow: auto;
   display: flex;
   flex-direction: column;
   height: 100%;
 `;
 
-const PageHeaderStyle = styled.div`
-  position: fixed;
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  height: 50px;
-  padding: 10px 120px 10px 40px;
-  background: #fff;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-  z-index: 1;
-`;
-
-const PageHeaderTitleStyle = styled.p`
-  font-size: 18px;
-  font-weight: 600;
-  margin: 0;
-  letter-spacing: -0.5px;
-`;
-
-const PageHeaderActionsStyle = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-end;
-  button {  margin-left: 10px; }
-  i { margin-right: 10px; }
-`;
-
-const PageContentStyle = styled.div`
+const PageContent = styled.div`
   // flex: 1;
   // overflow: hidden;
   margin: 90px 40px 40px 40px;
@@ -66,7 +39,7 @@ const PageContentStyle = styled.div`
   `};
 `;
 
-const PageFooterStyle = styled.div`
+const PageFooter = styled.div`
   position: fixed;
   width: 100%;
   bottom: 0;
@@ -74,41 +47,11 @@ const PageFooterStyle = styled.div`
   border-top: 1px solid rgba(0, 0, 0, 0.1);
 `;
 
-// Components
-const AppWrapper = props => <AppWrapperStyle>{props.children}</AppWrapperStyle>;
-
-const AppMainContent = props => (
-  <AppMainContentStyle>{props.children}</AppMainContentStyle>
-);
-
-const PageWrapper = props => (
-  <PageWrapperStyle>{props.children}</PageWrapperStyle>
-);
-
-const PageHeader = props => <PageHeaderStyle>{props.children}</PageHeaderStyle>;
-
-const PageHeaderTitle = props => (
-  <PageHeaderTitleStyle>{props.children}</PageHeaderTitleStyle>
-);
-
-const PageHeaderActions = props => (
-  <PageHeaderActionsStyle>{props.children}</PageHeaderActionsStyle>
-);
-
-const PageContent = props => (
-  <PageContentStyle bare={props.bare}>{props.children}</PageContentStyle>
-);
-
-const PageFooter = props => <PageFooterStyle>{props.children}</PageFooterStyle>;
-
 export {
   AppWrapper,
   AppMainContent,
   PageWrapper,
-  PageHeader,
-  PageHeaderTitle,
-  PageHeaderActions,
   PageContent,
   PageFooter,
+  ErrorLabel
 };
-
--- app/users/UserForm.jsx
+++ app/users/UserForm.jsx
@@ -1,19 +1,121 @@
-import React, { Component } from 'react';
-import { Formik, Field } from 'formik';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Field, withFormik } from 'formik';
+import * as Yup from 'yup';
+import { Part, Section, Row, Fields } from './UserStyle';
+import Button, { ButtonsGroup } from '../common/Button/Button';
+import { ErrorLabel } from '../common/Layout/Layout';
 
-class UserForm extends Component {
-  render() {
-    return (
-      <Formik
-        initialValues={{ email: '' }}
-        render={() => (
-          <form>
-            <Field type="email" name="email" placeholder="Email" />
-          </form>
-        )}
-      />
-    );
-  }
-}
+const UserSchema = Yup.object().shape({
+  firstName: Yup.string().required('first name is required'),
+  middleName: Yup.string().required('middle name is required'),
+  lastName: Yup.string().required('last name is required'),
+  address: Yup.string().required('address is required'),
+  age: Yup.number()
+    .min(18, 'age should be equal or above 18')
+    .required('age is required')
+});
 
-export default UserForm;
+const onSubmit = values => console.log('hello there', values);
+
+const formikEnhancer = withFormik({
+  validationSchema: UserSchema,
+  initialValues: { email: '' },
+  handleSubmit: onSubmit
+});
+
+const UserForm = props => {
+  const { touched, errors, handleSubmit } = props;
+
+  return (
+    <Section>
+      <form onSubmit={handleSubmit}>
+        <Part>
+          <Row>
+            <Fields>
+              <label className="itemLabel" htmlFor="firstName">
+                First Name *
+              </label>
+              <Field type="text" name="firstName" placeholder="first name" />
+              {errors.firstName && touched.firstName ? (
+                <ErrorLabel>{errors.firstName}</ErrorLabel>
+              ) : null}
+            </Fields>
+            <Fields>
+              <label className="itemLabel" htmlFor="middleName">
+                Middle Name*
+              </label>
+              <Field type="text" name="middleName" placeholder="middle name" />
+              {errors.middleName && touched.middleName ? (
+                <ErrorLabel>{errors.middleName}</ErrorLabel>
+              ) : null}
+            </Fields>
+            <Fields>
+              <label className="itemLabel" htmlFor="lastName">
+                Last Name *
+              </label>
+              <Field type="text" name="lastName" placeholder="last name" />
+              {errors.lastName && touched.lastName ? (
+                <ErrorLabel>{errors.lastName}</ErrorLabel>
+              ) : null}
+            </Fields>
+          </Row>
+          <Row>
+            <Fields>
+              <label className="itemLabel" htmlFor="address">
+                Address *
+              </label>
+              <Field type="text" name="address" placeholder="address" />
+              {errors.address && touched.address ? (
+                <ErrorLabel>{errors.address}</ErrorLabel>
+              ) : null}
+            </Fields>
+          </Row>
+          <Row>
+            <Fields>
+              <label className="itemLabel" htmlFor="age">
+                Age *
+              </label>
+              <Field type="number" name="age" placeholder="age" />
+              {errors.age && touched.age ? (
+                <ErrorLabel>{errors.age}</ErrorLabel>
+              ) : null}
+            </Fields>
+            <Fields>
+              <label className="itemLabel" htmlFor="gender">
+                Gender *
+              </label>
+              <Field component="select" name="gender">
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </Field>
+            </Fields>
+          </Row>
+          <ButtonsGroup>
+            <Button type="submit" success>
+              Submit
+            </Button>
+            <Button type="button" danger>
+              Clear
+            </Button>
+          </ButtonsGroup>
+        </Part>
+      </form>
+    </Section>
+  );
+};
+
+UserForm.propTypes = {
+  touched: PropTypes.shape({}),
+  errors: PropTypes.shape({}),
+  handleSubmit: PropTypes.func.isRequired
+};
+
+UserForm.defaultProps = {
+  touched: {},
+  errors: {}
+};
+
+const enhanceForm = formikEnhancer(UserForm);
+
+export default enhanceForm;
